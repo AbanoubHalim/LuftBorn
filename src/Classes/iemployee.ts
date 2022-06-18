3324fd5918998535e7ab1ce6c807246b3f6cffbe
ahalim@stingrayconsultation.com.test-google-a.com <ahalim@stingrayconsultation.com.test-google-a.com>
2022-06-18 15:11:14 +0200

    setup Project and add bootstrap

---
+++ src/Classes/iemployee.ts
@@ -0,0 +1,9 @@
+export interface IEmployee {
+        id: string;
+        age: number;
+        name: string;
+        phoneNumber: string;
+        address: string;
+        title: string;
+        jobDescription: string;
+}
